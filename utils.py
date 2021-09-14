9490873d05f0d525accd7f035f08ca5c5f04f6a8
spolovynko <spolovynko@gmail.com>
2021-09-14 13:08:23 +0200

    Update intents

--- utils.py
+++ utils.py
@@ -4,3 +4,11 @@ def tokenize(text):
     nlp = spacy.load("en_core_web_lg")
     doc = nlp(text)
     return doc
+
+def lemmatizer(doc):
+    return [token.lemma_ for token in doc]
+
+
+def remove_stopwords(doc):
+    stopwords = spacy.lang.en.stop_words.STOP_WORDS
+    return [token for token in doc if not token in stopwords]
